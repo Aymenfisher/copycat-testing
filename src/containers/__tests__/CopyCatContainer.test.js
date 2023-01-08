5e745d75ef45104a4fb708a1803ef7e729645e1e
Aymenfisher <fisheraymen@gmail.com>
2023-01-08 18:04:50 +0100

    added CopyCatContainer component tests

--- src/containers/__tests__/CopyCatContainer.test.js
+++ src/containers/__tests__/CopyCatContainer.test.js
@@ -6,9 +6,10 @@ import 'regenerator-runtime'
 
 //  Make all the imports below
 import React from 'react';
-import {screen,render} from '@testing-library/react';
+import { screen, render, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
+import { async } from 'regenerator-runtime';
 
 
 
@@ -19,12 +20,12 @@ test('Should display copied text', () => {
     render(
         <CopyCatContainer />
     )
-    
+
     const testValue = 'Hello World!';
 
     //query the input field and inserting a text into it
     const input = screen.getByRole('textbox')
-    userEvent.type(input,testValue);
+    userEvent.type(input, testValue);
     //query for the paragraph element
     const paragraph = screen.getByText(testValue)
     //assert
@@ -32,16 +33,56 @@ test('Should display copied text', () => {
 
 })
 
-test('Should remove copied text after removing tape', () => {
+test('Should remove copied text after removing tape', async () => {
     // Write your solution to task 11 within this test
+    render(
+        <CopyCatContainer />
+    )
 
+    const testValue = 'My mouth is shut';
 
+    //query the input field and inserting a text into it
+    const input = screen.getByRole('textbox')
+    userEvent.type(input, testValue);
+
+    //query for the paragraph element
+    let paragraph = screen.getByText(testValue)
+    //assert the paragraph exists
+    expect(paragraph).toBeInTheDocument();
+
+    //get the img element
+    const catImage = screen.getByAltText('copycat');
+    // click on image to get the text dissapear
+    userEvent.click(catImage);
+    await waitFor(() => {
+        paragraph = screen.queryByText(testValue);
+        expect(paragraph).toBeNull()
+    })
 
 })
 
-test('Should display copied text after removing tape', () => {
+test('Should display copied text after removing tape', async () => {
     // Write your solution to tasks 12-13 within this test
+    render(
+        <CopyCatContainer />
+    )
 
+    const testValue = 'Eventually this will appear';
+
+    //get the img element
+    const catImage = screen.getByAltText('copycat');
+    // click on image to get the text dissapear
+    userEvent.click(catImage);
+
+    // check if the quiet cat image exists
+    const catImageQuiet = await screen.findByAltText('quietcat')
+    expect(catImageQuiet).toBeInTheDocument();
+
+    // check if typing something will be hidden
+    const input = screen.getByRole('textbox')
+    userEvent.type(input, testValue);
+    const paragraph = screen.queryByText(testValue);
+    expect(paragraph).toBeNull();
 
 })
 
